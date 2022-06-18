ebf46516ec690aaeb1e217be3ab18a4396a04794
hectorchanht <hectorchanht@gmail.com>
2022-06-18 15:20:43 +0800

    unit HStack, extract LocaleAvatar

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -7,6 +7,23 @@ import { ReactNode } from 'react';
 import ThemeSwitchBtn from './themeSwitchBtn';
 const Links = ['History'];
 
+const LocaleAvatar = () => {
+  const router = useRouter();
+  const { locales, locale: activeLocale } = router;
+  const otherLocales = locales && locales.filter((locale) => locale !== activeLocale);
+
+  return otherLocales ? otherLocales.map((locale) => {
+      const { pathname, query, asPath } = router;
+      const isEn = activeLocale === 'en'
+      return (
+        <Link key={locale} href={{ pathname, query }} as={asPath} locale={locale}>
+          <a>
+            <Avatar h={10} name={isEn ? 'hk' : 'en'} src={isEn ? '/hk.svg' : '/en.svg'} />
+          </a>
+        </Link>
+      )
+    }) : null
+}
 
 const NavLink = ({ children }: { children: ReactNode }) => (
   <Link
@@ -22,26 +39,26 @@ const NavLink = ({ children }: { children: ReactNode }) => (
   </Link>
 );
 
+const HS = ({ children }) => <HStack
+  spacing={4}
+  display={{ base: 'none', md: 'flex' }}>
+  {children}
+</HStack >;
+
 const Menus = () => <HStack spacing={8} alignItems={'center'}>
   <Box as={Link} href="/">
     <Image src={'/logo.png'} width={40} height={40} />
   </Box>
 
-  <HStack
-    as={'nav'}
-    spacing={4}
-    display={{ base: 'none', md: 'flex' }}>
+  <HS>
     {Links.map((link) => (
       <NavLink key={link}>{link}</NavLink>
     ))}
-  </HStack>
+  </HS>
 </HStack>
 
 
 const Header = () => {
-  const router = useRouter()
-  const { locales, locale: activeLocale } = router
-  const otherLocales = locales && locales.filter((locale) => locale !== activeLocale)
 
   return (
     <div>
@@ -54,22 +71,15 @@ const Header = () => {
       <Flex as={'nav'} h={16} alignItems={'center'} justifyContent={'space-between'}>
         <Menus />
 
-        <ThemeSwitchBtn />
-
-        {otherLocales ? otherLocales.map((locale) => {
-          const { pathname, query, asPath } = router;
-          const isEn = activeLocale === 'en'
-          return (
-            <Link key={locale} href={{ pathname, query }} as={asPath} locale={locale}>
-              <a>
-                <Avatar h={10} name={isEn ? 'hk' : 'en'} src={isEn ? '/hk.svg' : '/en.svg'} />
-              </a>
-            </Link>
-          )
-        }) : null}
+        <HS>
+          <ThemeSwitchBtn />
+          <LocaleAvatar />
+        </HS>
       </Flex>
     </div>
   )
 }
+
+
 
 export default Header;
