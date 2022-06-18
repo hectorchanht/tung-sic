208ec133f1f30b3a87c8d3b52ccb169583e5e9ca
hectorchanht <hectorchanht@gmail.com>
2022-06-18 13:42:36 +0800

    Update header.tsx

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,35 +1,36 @@
-import { Box } from '@chakra-ui/react'
+import { Avatar, Box } from '@chakra-ui/react'
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { Avatar}  from '@chakra-ui/react'
- 
+
 const Header = () => {
   const router = useRouter()
   const { locales, locale: activeLocale } = router
   const otherLocales = locales && locales.filter((locale) => locale !== activeLocale)
 
   return (
-  <div>
-    <Head>
-      <title>tung-sic</title>
-      <meta name="description" content="tung sharing music 24/7" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <div>
+      <Head>
+        <title>tung-sic</title>
+        <meta name="description" content="tung sharing music 24/7" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    <Box as={'nav'}>
-     {otherLocales ? otherLocales.map((locale) => {
-    const { pathname, query, asPath } = router;
-       const isEn = activeLocale === 'en'
-    return <Link key={locale} href={{ pathname, query }} as={asPath} locale={locale}>
-      <a>
-        <Avatar name={isEn ? 'hk' : 'en'} src={isEn ? '/hk.svg' : '/en.svg'} />
-      </a>
-    </Link>
-  }) : null}
-    </Box>
-  </div>
-)}
+      <Box as={'nav'}>
+        {otherLocales ? otherLocales.map((locale) => {
+          const { pathname, query, asPath } = router;
+          const isEn = activeLocale === 'en'
+          return (
+            <Link key={locale} href={{ pathname, query }} as={asPath} locale={locale}>
+            <a>
+              <Avatar name={isEn ? 'hk' : 'en'} src={isEn ? '/hk.svg' : '/en.svg'} />
+            </a>
+            </Link>
+          )
+        }) : null}
+      </Box>
+    </div>
+  )
+}
 
 export default Header;
