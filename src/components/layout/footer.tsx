bbf112fa9d85ec00b4fe4a310f916caae23fe652
hectorchanht <hectorchanht@gmail.com>
2022-06-18 14:33:54 +0800

    textAlign={'center'}

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,6 +1,6 @@
 import { Box } from "@chakra-ui/react";
 
-const footer = () => <Box as="footer" mt="auto">
+const footer = () => <Box textAlign={'center'} as="footer" mt="auto">
   <a href="https://github.com/hectorchanht/tung-sic" target="_blank" rel="noopener noreferrer"        >
     Powered by hector
   </a>
