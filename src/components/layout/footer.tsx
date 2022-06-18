f2590bf431c7e8ac25c40e440bedc5c888d1e445
hectorchanht <hectorchanht@gmail.com>
2022-06-18 14:12:24 +0800

    https://github.com/hectorchanht/tung-sic

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@chakra-ui/react";
 
 const footer = () => <Box as="footer" mt="auto">
-  <a href="https://github.com/hectorchanht" target="_blank" rel="noopener noreferrer"        >
+  <a href="https://github.com/hectorchanht/tung-sic" target="_blank" rel="noopener noreferrer"        >
     Powered by hector
   </a>
 </Box>;
