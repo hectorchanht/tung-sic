e68e9818f24993e6f12bd861c5cd9bf0e25af423
hectorchanht <hectorchanht@gmail.com>
2022-06-18 12:42:07 +0800

    add some zh dict

--- src/components/faq/faqs.tsx
+++ src/components/faq/faqs.tsx
@@ -9,30 +9,23 @@ export default () => {
   const loc = useLocale();
 
   const faqList = Array.from({ length: 4 }, (_, i) => [loc.get(`faq-q${i + 1}`), loc.get(`faq-a${i + 1}`)]);
-  console.log(faqList)
   
-  return <Box>
+  return (
+    <Box>
     <Accordion allowMultiple>
-      {faqList.map(([q,a]) => (
-        <AccordionItem>
- 
-
- <h2>
-      <AccordionButton>
-        <Box flex='1' textAlign='left'>
-          {q}
-        </Box>
-        <AccordionIcon />
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4}>
-      {a}
-    </AccordionPanel>
-
-
-
-          </AccordionItem>
+      {faqList.map(([q,a],i) => (
+        <AccordionItem key={i}>
+          <h2>
+            <AccordionButton>
+              <Box flex='1' textAlign='left'>
+                {q}
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+        <AccordionPanel pb={4}>{a}</AccordionPanel>
+        </AccordionItem>
       ))}
-</Accordion>
-  </Box>
+    </Accordion>
+  </Box>)
 }
