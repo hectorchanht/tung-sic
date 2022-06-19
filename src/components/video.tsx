6a549e13e47fce499df0ec51350990ba4c26618b
hectorchanht <hectorchanht@gmail.com>
2022-06-19 12:15:10 +0800

    add videoInfo under videos if hover

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -1,8 +1,18 @@
 // https://gist.github.com/protrolium/8831763
+import { Box } from '@chakra-ui/react';
+import { isEmpty } from 'lodash';
 import Image from 'next/image';
+import React from 'react';
 
+const getYtInfo = (id = '') => fetch(`https://www.googleapis.com/youtube/v3/videos?id=${id}&key=${process.env.NEXT_PUBLIC_YOUTUBE_ID}&part=snippet`)
+  .then(d => d.json());
+// .then(d => {
+//   return d;
+// })
 
 const Video = ({ link }: { link: string }) => {
+  const [videoInfo, setVideoInfo] = React.useState({});
+
   if (!(['youtu.be', 'youtube.com'].some(domain => link.includes(domain)))) return null;
   link = link.replace('https://youtu.be/', 'https://www.youtube.com/watch?v=');
 
@@ -10,9 +20,25 @@ const Video = ({ link }: { link: string }) => {
   const id = link.split('v=').pop().slice(0, 11);
 
   return (
-    <a href={link} target="_blank" rel="noreferrer">
-      <Image height={352} width={470} src={`https://img.youtube.com/vi/${id}/0.jpg`} />
-    </a>
+    <Box>
+      <a onMouseOver={() => isEmpty(videoInfo) && getYtInfo(id).then(setVideoInfo)} href={link} target="_blank" rel="noreferrer">
+        <Image height={352} width={470} src={`https://img.youtube.com/vi/${id}/0.jpg`} alt={'youtube thumbnail'} />
+      </a>
+      {!isEmpty(videoInfo) && (
+        <Box>
+          <Box>
+            {/* @ts-ignore */}
+            {videoInfo?.items?.[0]?.snippet?.title}
+          </Box>
+          <Box>
+            {/* @ts-ignore */}
+            @{videoInfo?.items?.[0]?.snippet?.channelTitle}
+            {' '}
+            {/* @ts-ignore */}
+            ({videoInfo?.items?.[0]?.snippet?.publishedAt.slice(0, 10)})
+          </Box>
+        </Box>)}
+    </Box>
   )
 };
 
