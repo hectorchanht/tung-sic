f3f87c40ddfd6053af3d93d39a5ba41dd567b38f
hectorchanht <hectorchanht@gmail.com>
2022-06-28 11:23:43 +0800

    replace Marquee by gradient btn

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,10 @@
 import { Box, Center } from '@chakra-ui/react'
 import type { NextPage } from 'next'
-import Marquee from "react-fast-marquee"
 import FAQ from '../src/components/faq'
 import GradientBtn from '../src/components/gradientBtn'
 import Layout from '../src/components/layout'
 import { useLang, useLocale } from '../src/utils/hooks'
 
-
-const M = ({ isEn, loc }) => <Marquee direction="left" pauseOnHover pauseOnClick gradient={false}>
-  <a href='https://chat.whatsapp.com/Ek2082QfodUAY00Qdcr2Di' target="_blank" rel="noreferrer" >
-    {/* <Image width={420} height={230} src={`/${isEn ? 'en' : 'zh'}-join.svg`} alt={'join whatsapp group now!'} /> */}
-    <Box textOverflow={'ellipsis'} fontSize={30}>
-      ✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨{isEn && '✨✨✨'}
-      <br />
-      {' '}{loc.get('join')}
-      <br />
-      ✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨{isEn && '✨✨✨'}
-    </Box>
-  </a>
-</Marquee>;
 
 const Home: NextPage = () => {
   const loc = useLocale();
@@ -34,11 +20,11 @@ const Home: NextPage = () => {
         <br />
         <GradientBtn href={'https://chat.whatsapp.com/Ek2082QfodUAY00Qdcr2Di'}>
           <Box textOverflow={'ellipsis'} fontSize={30}>
-            ✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨{isEn && '✨✨✨'}
+            ✨✨✨✨✨✨✨✨✨✨✨✨✨{isEn && '✨✨✨'}
             <br />
             {' '}{loc.get('join')}
             <br />
-            ✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨{isEn && '✨✨✨'}
+            ✨✨✨✨✨✨✨✨✨✨✨✨✨{isEn && '✨✨✨'}
           </Box>
         </GradientBtn>
         <br />
@@ -62,4 +48,19 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export default Home;
+
+// import Marquee from "react-fast-marquee";
+
+// const M = ({ isEn, loc }: { isEn: boolean, loc: any }) => <Marquee direction="left" pauseOnHover pauseOnClick gradient={false}>
+//   <a href='https://chat.whatsapp.com/Ek2082QfodUAY00Qdcr2Di' target="_blank" rel="noreferrer" >
+//     {/* <Image width={420} height={230} src={`/${isEn ? 'en' : 'zh'}-join.svg`} alt={'join whatsapp group now!'} /> */}
+//     <Box textOverflow={'ellipsis'} fontSize={30}>
+//       ✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨{isEn && '✨✨✨'}
+//       <br />
+//       {' '}{loc.get('join')}
+//       <br />
+//       ✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨{isEn && '✨✨✨'}
+//     </Box>
+//   </a>
+// </Marquee>;
