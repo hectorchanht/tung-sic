3172b68f9ea811accf5db8197b122e4b84546933
hectorchanht <hectorchanht@gmail.com>
2022-06-28 11:13:59 +0800

    using GradientBtn for join and yt playlist

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Center } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import Marquee from "react-fast-marquee"
 import FAQ from '../src/components/faq'
+import GradientBtn from '../src/components/gradientBtn'
 import Layout from '../src/components/layout'
 import { useLang, useLocale } from '../src/utils/hooks'
 
+
+const M = ({ isEn, loc }) => <Marquee direction="left" pauseOnHover pauseOnClick gradient={false}>
+  <a href='https://chat.whatsapp.com/Ek2082QfodUAY00Qdcr2Di' target="_blank" rel="noreferrer" >
+    {/* <Image width={420} height={230} src={`/${isEn ? 'en' : 'zh'}-join.svg`} alt={'join whatsapp group now!'} /> */}
+    <Box textOverflow={'ellipsis'} fontSize={30}>
+      ✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨{isEn && '✨✨✨'}
+      <br />
+      {' '}{loc.get('join')}
+      <br />
+      ✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨{isEn && '✨✨✨'}
+    </Box>
+  </a>
+</Marquee>;
 
 const Home: NextPage = () => {
   const loc = useLocale();
@@ -16,9 +30,9 @@ const Home: NextPage = () => {
         {loc.get('welcome')}
       </Box>
 
-      <Marquee direction="left" pauseOnHover pauseOnClick gradient={false}>
-        <a href='https://chat.whatsapp.com/Ek2082QfodUAY00Qdcr2Di' target="_blank" rel="noreferrer" >
-          {/* <Image width={420} height={230} src={`/${isEn ? 'en' : 'zh'}-join.svg`} alt={'join whatsapp group now!'} /> */}
+      <Center>
+        <br />
+        <GradientBtn href={'https://chat.whatsapp.com/Ek2082QfodUAY00Qdcr2Di'}>
           <Box textOverflow={'ellipsis'} fontSize={30}>
             ✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨{isEn && '✨✨✨'}
             <br />
@@ -26,15 +40,20 @@ const Home: NextPage = () => {
             <br />
             ✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨{isEn && '✨✨✨'}
           </Box>
-        </a>
-      </Marquee>
+        </GradientBtn>
+        <br />
+      </Center>
+
+      <Center>
+        <GradientBtn cls={'btn2'} href={'https://www.youtube.com/playlist?list=PLAXM729RypVMDM4leAk_rycoNBGIk-SwY'}>
+          Youtube Playlist
+        </GradientBtn>
+      </Center>
 
       <Box fontWeight={800} textAlign='center'>
         {loc.get('intro1')}
       </Box>
 
-      <br />
-      <br />
       <br />
 
       <FAQ />
